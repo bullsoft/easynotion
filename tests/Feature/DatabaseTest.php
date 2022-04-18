6c3ff0b5e436f5cc96c20a5712c0fcff5fdffef9
Gu Weigang <guweigang@bullsoft.org>
2022-04-18 22:58:55 +0800

    setup $dbId

--- tests/Feature/DatabaseTest.php
+++ tests/Feature/DatabaseTest.php
@@ -3,22 +3,24 @@ use EasyNotion\Http\Database as DbClient;
 use EasyNotion\Entity\Database as DbEntity;
 use EasyNotion\Common\UUIDv4;
 
-beforeEach(fn () => $this->client = $this->notion->database());
+
+beforeEach(function() {
+    $this->dbId = '3791ea11e83a4f979a744f043fefdf37';
+    $this->client = $this->notion->database();
+});
 
 test('database client instanceof EasyNotion\Http\Database', function () {
     expect($this->client)->toBeInstanceOf(DbClient::class);
 });
 
 test('database entity instanceof EasyNotion\Entity\Database', function() {
-    $dbId = '3791ea11e83a4f979a744f043fefdf37';
-    $db = $this->client->get($dbId);
+    $db = $this->client->get($this->dbId);
     expect($db)->toBeInstanceOf(DbEntity::class);
 });
 
 test('database id instanof UUIDv4', function() {
-    $dbId = '3791ea11e83a4f979a744f043fefdf37';
-    $db = $this->client->get($dbId);
+    $db = $this->client->get($this->dbId);
     expect($db->id())->toBeInstanceOf(UUIDv4::class);
-    expect($db->id()->get())->toBe(UUIDv4::format($dbId));
+    expect($db->id()->get())->toBe(UUIDv4::format($this->dbId));
 });
 
